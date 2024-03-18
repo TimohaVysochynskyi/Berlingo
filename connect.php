695d6552690e7cc16941bc995404726296e29a86
Timoha Vysochynskyi <timohavysach@gmail.com>
2024-03-18 23:02:44 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,9 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "berlingo");
+
+//$conn = new mysqli("localhost", "root", "", "berlingo");
+
+$conn = new mysqli("localhost", "berlingo_admin", "B0r0nym0_\$v0iE", "berlingo_berlingo");
+
 if ($conn->connect_error) {
     die("Connection failed");
 }
